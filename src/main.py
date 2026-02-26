32a303ee34bd19cd57acc0746211adf1e1a99a7b
madijonovsardorbek544-cmyk <madijonovsardorbek544@gmail.com>
2026-02-26 23:47:41 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,5 @@
-
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi import Request
-
-app = FastAPI(title="POLARIS Intel")
-
-templates = Jinja2Templates(directory="src/templates")
-
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
 
 app = FastAPI()
 
